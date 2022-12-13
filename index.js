c0f47f052ad41253141783fdec58abda6e44ad3d
David Rajcher <davidrajcher@Davids-MacBook-Pro-2.local>
2022-12-13 17:45:37 +0100

    remove package json

--- index.js
+++
@@ -1,8 +0,0 @@
-const characters = require("./characters.json");
-const express = require("express");
-
-const app = express();
-
-const PORT = 5000;
-
-app.listen(PORT, () => console.log(`listening to port: ${PORT}`));
